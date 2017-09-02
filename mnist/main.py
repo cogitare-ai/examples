bfdb3c7e89b9dee298a5573dbceb25b5fd7ede61
Aron Bordin <aron.bordin@gmail.com>
2017-09-02 20:14:15 -0300

    update to use dataholder

--- mnist/main.py
+++ mnist/main.py
@@ -3,16 +3,16 @@ import torch.optim as optim
 import pprint
 import cogitare
 from cogitare.models.linear.logistic import LogisticRegression
-from cogitare.dataset import Dataset
+from cogitare.data import DataSet
 from sklearn.datasets import fetch_mldata
 
 
 # Configurations
 
-parser = argparse.ArgumentParser(description='PyTorch MNIST Model')
+parser = argparse.ArgumentParser(description='Cogitare PyTorch MNIST Model')
 
 parser.add_argument('--batch-size', help='Size of the training batch', type=int, default=64, metavar='N')
-parser.add_argument('--cuda', help='enable cuda', action='store_true', default=False)
+parser.add_argument('--cuda', help='enable cuda', action='store_true')
 parser.add_argument('--learning-rate', help='Learning rate', type=float, default=0.1, metavar='N')
 parser.add_argument('--model', help='select the model to train', choices=['logistic', 'mlp'], default='logistic')
 parser.add_argument('--momentum', help='momentum', type=float, default=0.0, metavar='N')
@@ -28,9 +28,10 @@ cogitare.seed(args.seed)
 # Data
 mnist = fetch_mldata('MNIST original')
 mnist.data = mnist.data / 255
-data = Dataset(mnist.data, mnist.target.astype(int), batch_size=args.batch_size)
+data = DataSet([mnist.data, mnist.target.astype(int)], batch_size=args.batch_size)
 
 # Model
 l = LogisticRegression(input_size=784, num_classes=10, cuda=args.cuda)
+l.register_default_plugins()
 optimizer = optim.SGD(l.parameters(), lr=args.learning_rate, momentum=args.momentum)
 l.learn(data, optimizer)
